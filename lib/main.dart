490f7b540a76c6e9bf75f3c1be8f7dfbeba3f5f4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-03 21:24:12 +0100

    Added the phone and email  tiles

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,51 @@ class MyApp extends StatelessWidget {
                 letterSpacing: 2.5,
                 color: Colors.teal[100],
               ),
-            )
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '+234 8064 2805',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.email, color: Colors.teal),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'chief@chief.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),

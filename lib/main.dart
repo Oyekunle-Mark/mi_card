8e05417967b349d1f3298a804b1b01ea8f672630
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-03 20:19:55 +0100

    Added new layout containers

--- lib/main.dart
+++ lib/main.dart
@@ -10,32 +10,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.white,
-              child: Text('Container 1'),
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              color: Colors.blue,
-              child: Text('Container 2'),
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              color: Colors.red,
-              child: Text('Container 3'),
-            )
-          ],
-        )),
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Row(children: [
+          Container(
+            width: 100.0,
+            color: Colors.red,
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                height: 100.0,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+              ),
+            ],
+          ),
+          Container(
+            width: 100.0,
+            color: Colors.blue,
+          )
+        ]),
       ),
-    );
+    ));
   }
 }
